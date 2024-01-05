5aacb5e4f9e8f2b9eda0d4c5764cdce87f426715
Pedro-Henrique771 <pedrohr0715@gmail.com>
2024-01-05 11:28:06 -0300

    Carrossel quase pronto

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,5 @@
+// Carrossel pt de cima (auto)
+
 let slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -19,3 +21,39 @@ function prevSlide() {
   document.querySelector('.prev').addEventListener('click', prevSlide);
 
 setInterval(nextSlide, 10000); // Muda de slide a cada 10 segundos (10000 milissegundos)
+
+// Carrossel parte de baixo (manual)
+
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
